63b58c6a4abe47de90487b0a60f4fddc14641cea
Tarek Ali <tarekali15@outlook.com>
2021-03-30 16:43:12 -0500

    setup function

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module remote-buggy
 
 go 1.13
+
+require github.com/stianeikeland/go-rpio/v4 v4.4.0
--- utils/hardware/hardware.go
+++ utils/hardware/hardware.go
@@ -1,1 +1,16 @@
 package hardware
+
+import (
+	"github.com/stianeikeland/go-rpio/v4"
+
+	"log"
+)
+
+func Setup() {
+	err := rpio.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
