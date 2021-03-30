46abddb4782797cd4c4edf1ba100b74ec885ffb8
Tarek Ali <tarekali15@outlook.com>
2021-03-30 17:07:41 -0500

    external declarations. Going to definitely change this code structure

--- utils/hardware/hardware.go
+++ utils/hardware/hardware.go
@@ -6,11 +6,23 @@ import (
 	"log"
 )
 
+var speedA rpio.Pin
+var speedB rpio.Pin
+var clockwiseA rpio.Pin
+var clockwiseB rpio.Pin
+var counterClockwiseA rpio.Pin
+var counterClockwiseB rpio.Pin
+
 func Setup() {
 	err := rpio.Open()
+
+	speedA = rpio.Pin(26)
+	speedB = rpio.Pin(11)
+	clockwiseA = rpio.Pin(19)
+	counterClockwiseA = rpio.Pin(13)
+	clockwiseB = rpio.Pin(6)
+	counterClockwiseB = rpio.Pin(5)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
-
