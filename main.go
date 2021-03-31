7a04310deab0740731417a4d5f20cd27cec4e512
Tarek Ali <tarekali15@outlook.com>
2021-03-30 20:17:28 -0500

    removed concurrent checks

--- main.go
+++ main.go
@@ -1,24 +1,10 @@
 package main
 
 import (
-	"os"
-	"os/signal"
 	"remote-buggy/utils/hardware"
-	"syscall"
-
-	"github.com/stianeikeland/go-rpio"
 )
 
 func main() {
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		rpio.Close()
-		os.Exit(1)
-	}()
-	hardware.Setup()
-
 	for {
 		hardware.Forward()
 	}
